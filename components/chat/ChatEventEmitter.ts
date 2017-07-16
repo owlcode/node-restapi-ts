eaa9402c1b9038a3715f3117e35150fe10e0de9f
Dawid Sowa <dawidpawelsowa@gmail.com>
2017-07-17 00:48:50 +0200

    fix

--- components/chat/ChatEventEmitter.ts
+++
@@ -1,9 +0,0 @@
-import {EventEmitter} from "events";
-import {IMessage} from "../../api/message/message.model";
-
-export default class ChatEventEmitter extends EventEmitter {
-    newMessage(message: IMessage) {
-        this.emit("new-message");
-    }
-}
-
